3a81b221a01ada0396901e006951eda1c81be173
Hew Ingram <hewingram@Hews-MacBook-Pro.local>
2023-05-05 21:04:12 +0100

    update image

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Fresh App</title>
+        <title>Morris bells</title>
         <html style={{ margin: 0 }}></html>
         <body style={{ margin: 0 }}></body>
       </Head>
       <div style="width: 100vw; height: 100vh; background-color: blue; padding: 18px; background-color: #202124; display: flex; flex-direction: column; align-items: center; justify-content: center">
         <img
-          src="/53b8806b89ad80a58a54924fe8e8be84f7f8.png"
+          src="/bells.png"
           width="128"
           height="128"
           alt="bells"
